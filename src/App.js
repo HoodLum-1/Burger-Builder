30e9ef14e089776515d62648cfb34e651304b277
Malesela Sithole <malesela.ms@gmail.com>
2020-09-11 15:44:05 +0200

    02-after-ingredients

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div>
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder></BurgerBuilder>
+        </Layout>
       
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
